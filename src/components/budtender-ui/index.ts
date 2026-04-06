58575811d26be83dee413f2222c451c2f624fa29
Medusa <devoye.maysson@gmail.com>
2026-04-06 16:56:42 +0200

    feat: export BudTenderWidget and its props from new components index file

--- src/components/budtender-ui/index.ts
+++ src/components/budtender-ui/index.ts
@@ -1,9 +1,2 @@
 export { default as BudTenderWidget } from './BudTenderWidget';
-export { default as BudTenderMessage } from './BudTenderMessage';
-export { default as BudTenderTypingIndicator } from './BudTenderTypingIndicator';
-export { default as BudTenderFeedback } from './BudTenderFeedback';
-export { default as BudTenderBackground } from './BudTenderBackground';
-
 export type { BudTenderWidgetProps } from './BudTenderWidget';
-export type { BudTenderMessageProps } from './BudTenderMessage';
-export type { BudTenderFeedbackProps } from './BudTenderFeedback';
